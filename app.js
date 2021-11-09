132a667f5cd790e04dcbec8748910cc0b6bc6c9b
alvaro0106 <alvarollvv@gmail.com>
2021-11-09 05:05:39 -0600

    servir archivos html

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ let app = express()
 
 app.listen(3000)
 
-app.get('/hello',(request, response)=>{
-    response.send('hello express')
-
-});
+ console.log(__dirname);
 
 app.get("/", function(req, res) 
-{ res.send('Hello Express'); });
-
+ { res.sendFile(__dirname + '/index.html'); });

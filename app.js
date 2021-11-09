5c0baacdda41701ddabb84f49ccf716c0299fd38
alvaro0106 <alvarollvv@gmail.com>
2021-11-09 06:00:37 -0600

    Serve Static Assets

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ app.listen(3000)
  console.log(__dirname);
 
 app.get("/", function(req, res) 
- { res.sendFile(__dirname + '/index.html'); });
+ { res.sendFile(__dirname + '/views/index.html'); });
+
+app.use('/public', express.static(__dirname + '/public'))
